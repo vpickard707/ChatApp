dc1cac03dbf7618553c211bf120211ad592d422d
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-03-16 16:18:53 -0700

    updating client files, style and finishing touches

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import config from "./config";
+import io from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import BottomBar from "./BottomBar";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chat: [],
+      content: "",
+      name: "",
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io(config[process.env.NODE_ENV].endpoint);
+
+    // Load the last 10 messages in the window.
+    this.socket.on("init", (msg) => {
+      let msgReversed = msg.reverse();
+      this.setState(
+        (state) => ({
+          chat: [...state.chat, ...msgReversed],
+        }),
+        this.scrollToBottom
+      );
+    });
+
+    // Update the chat if a new message is broadcasted.
+    this.socket.on("push", (msg) => {
+      this.setState(
+        (state) => ({
+          chat: [...state.chat, msg],
+        }),
+        this.scrollToBottom
+      );
+    });
+  }
+
+  // Save the message the user is typing in the input field.
+  handleContent(event) {
+    this.setState({
+      content: event.target.value,
+    });
+  }
+
+  //
+  handleName(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    // Prevent the form to reload the current page.
+    event.preventDefault();
+
+    // Send the new message to the server.
+    this.socket.emit("message", {
+      name: this.state.name,
+      content: this.state.content,
+    });
+
+    this.setState((state) => {
+      // Update the chat with the user's message and remove the current message.
+      return {
+        chat: [
+          ...state.chat,
+          {
+            name: state.name,
+            content: state.content,
+          },
+        ],
+        content: "",
+      };
+    }, this.scrollToBottom);
+  }
+
+  // Always make sure the window is scrolled down to the last message.
+  scrollToBottom() {
+    const chat = document.getElementById("chat");
+    chat.scrollTop = chat.scrollHeight;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Paper id="chat" elevation={3}>
+          {this.state.chat.map((el, index) => {
+            return (
+              <div key={index}>
+                <Typography variant="caption" className="name">
+                  {el.name}
+                </Typography>
+                <Typography variant="body1" className="content">
+                  {el.content}
+                </Typography>
+              </div>
+            );
+          })}
+        </Paper>
+        <BottomBar
+          content={this.state.content}
+          handleContent={this.handleContent.bind(this)}
+          handleName={this.handleName.bind(this)}
+          handleSubmit={this.handleSubmit.bind(this)}
+          name={this.state.name}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
